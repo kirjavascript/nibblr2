67aa7313bc9ffe86695ffe695e30903a945cd2b4
kirjavascript <snkenjoi@gmail.com>
2020-06-14 17:27:32 +0100

    savestate

--- base/config.js
+++ base/config.js
@@ -1,10 +1,79 @@
 const { watch } = require('chokidar');
+const { readFile } = require('fs').promises;
+const path = require('path');
+const initial = require('../config.json');
+const configPath = path.join(__dirname, '../config.json');
 
-// config.server[0].get('asd');
+const reload = async () => {
+    try {
+        return JSON.parse(await readFile(configPath))
+    } catch (e) {
+        console.error(`problem reading config file: ${e.message}`);
+    }
+};
 
-watch(__dirname + '/../config.json')
+function patch() {
+
+}
+
+// if (key === "password") return value.replace(/./g, '*')
+
+watch(configPath)
     .on('change', () => {
-        console.log('change');
+        patch(config, reload());
     });
 
-// -> set a new root node / channel / server
+
+module.exports = () => {
+    const store = {
+        test: true
+    };
+    const path = [store];
+
+    const ctx = (local, path = []) => {
+
+        return {
+            ctx,
+            get,
+        };
+    };
+
+    const getContext = (ctx, store) => {
+
+        return {
+            ctx: () => {
+            },
+            root: () => {
+            },
+            get: () => {
+            },
+
+        };
+    };
+
+
+
+    return { get, ctx };
+
+    // config.ctx('servers').ctx('3')
+
+    // inheritance
+    // data sanitization
+    // computed values
+
+    // get all set sudo
+    // isSudo check
+    // config.safe()
+    // config.onChange
+    // .bindGetter()
+
+    // map array methods? object methods?
+    // -> set a new root node / channel / server
+    //
+    // .getBase
+};
+
+const config = module.exports();
+
+
+console.log(config.get())
